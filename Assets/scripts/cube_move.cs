e7366cdf66ba45c25600c1f2b70f606a0a1617d5
yoavh <yoav.hops@gmail.com>
2015-07-24 14:05:18 +0300

    initial connit

---
+++ Assets/scripts/cube_move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube_move : MonoBehaviour {
+
+    public float try_to_move = 1.0f;
+    private float try_to_move_tmp = 1.0f;
+    public float add_force = 1.0f;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        try_to_move_tmp -= Time.deltaTime;
+        if (try_to_move_tmp < 0)
+        {
+            try_to_move_tmp = try_to_move;
+            move();
+        }
+
+    }
+
+    void move()
+    {
+        rb.AddForce(add_force * Vector3.right );
+    }
+
+}
