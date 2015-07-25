c76c7153ddcdea8a5a00ee62e6c3704fdbeaec14
idoadler <ido.adler@mail.huji.ac.il>
2015-07-25 14:39:23 +0300

    score

--- Assets/scripts/hero_move.cs
+++ Assets/scripts/hero_move.cs
@@ -6,14 +6,18 @@ public class hero_move : MonoBehaviour {
     //Variables
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
+    public float rotationSpeed = 300;
     public float gravity = 20.0F;
     public GameObject model;
     private Vector3 moveDirection = Vector3.zero;
 
-    float rotation = 0;
     Quaternion lastDirection = Quaternion.identity;
     void Update()
     {
+        // rotate dice
+        model.transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * Input.GetAxis("Vertical"), Space.World);
+        model.transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Space.World);
+        
         CharacterController controller = GetComponent<CharacterController>();
         // is the controller on the ground?
         if (controller.isGrounded)
@@ -21,18 +25,6 @@ public class hero_move : MonoBehaviour {
             //Feed moveDirection with input.
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
-            if (moveDirection != Vector3.zero)
-            {
-                rotation += speed * 100 * Time.deltaTime;
-                lastDirection = Quaternion.LookRotation(moveDirection, Vector3.up);
-                model.transform.rotation = lastDirection;
-                model.transform.Rotate(Vector3.right, rotation);
-            }
-            else
-            {
-                rotation = 0;
-                model.transform.rotation = lastDirection;
-            }
             //Multiply it by speed.
             moveDirection *= speed;
             //Jumping
