2d657835a60e8370ba0fa29faed803c3b24632be
yoavh <yoav.hops@gmail.com>
2015-07-25 14:32:01 +0300

    hey

---
+++ Assets/editor/ObjectBuilderEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(find_top_edge))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        find_top_edge myScript = (find_top_edge)target;
+        if (GUILayout.Button("save rotation"))
+        {
+            myScript.save_rotation();
+        }
+    }
+}
