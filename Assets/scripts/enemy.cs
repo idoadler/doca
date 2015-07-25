af13f511e9fcb02485126fcfe3048f8920880e31
idoadler <ido.adler@mail.huji.ac.il>
2015-07-25 15:02:15 +0300

    enemy script

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+    public int health;
+
+    public int attack()
+    {
+        // if enemy attacking return attack power
+        // else return 0
+        return 0;
+        // update hit matter
+    }
+
+    public void getHit(int damage)
+    {
+        health -= damage;
+        // if health < 0 die
+        // update health matter
+    }
+}
